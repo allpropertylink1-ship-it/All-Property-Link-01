a3e15f528b04bb1f9e38b33a8aafa412617e838d
All-Property-Link-Dev <user@allpropertylink.com>
2026-09-01 10:55:46 +0300

    refactor: unify mobile nav - drawer nav-only, desktop avatar dropdown, video-only PWA launch

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,14 +55,6 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="All Property Link" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <link rel="apple-splash-screen" href="/icons/splash-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-splash-screen" href="/icons/splash-750x1334.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-splash-screen" href="/icons/splash-828x1792.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-splash-screen" href="/icons/splash-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" />
-        <link rel="apple-splash-screen" href="/icons/splash-1242x2688.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" />
-        <link rel="apple-splash-screen" href="/icons/splash-1536x2048.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-splash-screen" href="/icons/splash-1668x2388.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-splash-screen" href="/icons/splash-2048x2732.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" />
       </head>
       <body className={`${sora.variable} ${dmSans.variable} flex min-h-screen flex-col antialiased`}>
         <AuthProvider>
